ff3caf057f3fcc82a74e702307ebc72d75df4443
HCross <haydenlcrossjunk@gmail.com>
2020-01-25 21:25:25 -0800

    trying to push to heroku

---
+++ server.js
@@ -0,0 +1,19 @@
+//dependencies 
+const path = require("path");
+const express = require("express");
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routing code for the different pages
+app.use( '/', (req, res) => {
+    res.send("/11-Express/Develop/assets/index.html")
+});
+
+app.use( '/notes', (req, res) => {
+    res.send("/11-Express/Develop/assets/notes.html")
+});
